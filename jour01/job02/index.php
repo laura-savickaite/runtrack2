3741d40a7a373614e7079ad4cdd65b67ca0227b8
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-02 11:10:00 +0100

    le job02 avec deux echo différents et deux comments différents

---
+++ jour01/job02/index.php
@@ -0,0 +1,8 @@
+<?php
+ $var1 = "Hello LaPlateforme!";
+
+ //le premier echo affiche la valeur (string) de ma variable var1
+ echo $var1;
+ //le second echo est une string directement écrite à la commande echo
+ echo "Hello LaPlateforme";
+?>
