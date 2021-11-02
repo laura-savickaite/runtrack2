78f09d8285786ee55ca75c8711f02b748cd01fb8
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-02 18:37:21 +0100

    ajout d'une explication pour le modulo

--- jour02/job4/index.php
+++ jour02/job4/index.php
@@ -2,7 +2,7 @@
 $index=1;
 
 while ($index<=100){
-
+    // == 0 car on parle du RESTE de la division ex: 14/5 = 2 -- 14-10= 4 (mon reste de la division) donc =/= 0 ainsi mon ordinateur ne prend pas en compte 14 et passe au suivant en suivant la même méthode de diviser
         if (($index%5 == 0) && ($index%3 == 0)) {
            echo ("FizzBuzz <br/>");
          } elseif ($index%3 == 0) {
