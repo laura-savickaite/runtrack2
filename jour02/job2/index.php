885e04fb0001ddebeef94af2322fa3df5557ba85
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-02 16:43:55 +0100

    job2 index complet

---
+++ jour02/job2/index.php
@@ -0,0 +1,13 @@
+<?php
+$index=0;
+
+while($index <= 1337){
+ 
+            if(!($index==26 || $index==37|| $index==88 || $index==1111)) { 
+                echo "$index <br />";
+             } 
+        $index++;
+}
+
+?>
+
