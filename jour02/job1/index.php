0504c27885411bbbe3500f42b4b43a29c2535f28
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-02 16:04:09 +0100

    job1 du jour2

---
+++ jour02/job1/index.php
@@ -0,0 +1,16 @@
+<?php 
+    $index=0;
+
+    while($index <= 1337) {
+        
+        if($index==42) {
+            echo "<b><u>$index</u></b><br />";
+        }else{
+            echo "$index <br />";
+        }
+
+        $index++;
+    }
+
+    
+?>
