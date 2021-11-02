b0a802e98a49964e1837256b30b89056cbd536cf
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-02 19:11:15 +0100

    essai du job 5 mais fail à retenter

---
+++ jour02/job5/index.php
@@ -0,0 +1,13 @@
+<?php
+$index=0;
+
+while ($index <= 1000) {
+
+    // if (($index%1 == 0) && ($index%$index == 0)){
+    //     echo "help <br/>";
+    // }
+    echo "$index <br/>";
+    $index++;
+}
+
+?>
