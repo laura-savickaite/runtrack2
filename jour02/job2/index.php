256572f5276c5dcfd2e02b18e97e9ff0fba28c8a
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-03 11:02:24 +0100

    modification

--- jour02/job2/index.php
+++ jour02/job2/index.php
@@ -4,7 +4,7 @@ $index=0;
 while($index <= 1337){
  
             if(!($index==26 || $index==37|| $index==88 || $index==1111)) { 
-                echo "$index <br />";
+                echo "$index<br />";
              } 
         $index++;
 }
--- jour02/job5/index.php
+++ jour02/job5/index.php
@@ -1,13 +1,15 @@
 <?php
-$index=0;
+    $index=0;
 
-while ($index <= 1000) {
+//while ($index <= 1000) {
 
     // if (($index%1 == 0) && ($index%$index == 0)){
     //     echo "help <br/>";
     // }
-    echo "$index <br/>";
-    $index++;
-}
+   // echo "$index <br/>";
+  //  $index++;
+//}
+
+
 
 ?>
