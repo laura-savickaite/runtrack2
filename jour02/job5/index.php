972e0cd16ff5823b2868d44c958ccf54a2d6d98c
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-03 18:59:59 +0100

    tentative avec while et différentes méthodes

--- jour02/job5/index.php
+++ jour02/job5/index.php
@@ -3,7 +3,7 @@
 for($index=2; $index <= 1000; $index++) {
     $nbdiviseurs=0;
     
-
+//si jindex = 2 alors nbdiviseur = 1 car une seule condition est donnée, si jIndex=1 alors il est divisible par 1 ET LUI-MM donc 2nb de diviseurs (lui-mm et 1) et vice versa
     for($jIndex=2; $jIndex<=$index; $jIndex++){
         if ($index % $jIndex==0) {
             $nbdiviseurs++;
@@ -14,4 +14,27 @@ for($index=2; $index <= 1000; $index++) {
             echo $index."</br>";
         }
 }
+
+
+// $index=2;
+// $jIndex=1;
+// $nbdiviseurs=0;
+
+// while ($index <= 1000) {
+//     while ($jIndex<=$index) {
+
+//         if ($index % $jIndex==0) 
+//             $nbdiviseurs++;
+        
+//             $jIndex++;
+        
+//     }
+
+//     if ($nbdiviseurs==2){
+//         echo $index."<br/>";
+//     }
+//     $jIndex=1;
+//     $nbdiviseurs=0;
+//     $index++;
+// }
 ?>
