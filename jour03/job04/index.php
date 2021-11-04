8ce562dd42ba10d81a50aa373007237232380177
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-04 13:53:09 +0100

    job04 terminé

--- jour03/job04/index.php
+++ jour03/job04/index.php
@@ -1,12 +1,14 @@
 <?php
 $str="Dans l'espace, personne ne vous entend crier";
 
-$index=0; //c'est mon index counter de char
+//$index=0; //c'est mon index counter de char
 
-// var_dump ($str); //à titre informatif pour savoir cb de char il y a dans ma string (pour mon résultat final)
+// var_dump ($str); //à titre informatif pour savoir cb de char il y a dans ma string (pour mon résultat final) = 44
 
-for($index=0; $str[$index]==true; $index++){
-    echo $str[$index]."<br/>";
+while (isset($str[$index])==true) {
+    $index++;
 }
+
+echo $str. "<br/>" . $index;
 
 ?>
