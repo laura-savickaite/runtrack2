8385567e510579bec88171c0ed69b026fa9fe919
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-04 16:30:02 +0100

    job 5 petite astuce rajoutée

--- jour03/job05/index.php
+++ jour03/job05/index.php
@@ -5,10 +5,21 @@ $str="On n'est pas le meilleur quand on le croit mais quand on le sait";
 $index=0; //mon index counter
 
 $dic=array (
-    "consonnes"=>["n", "t", "p", "s", "l", "m", "r", "q", "d"],
+    "consonnes" => ["n","t", "p", "s", "l", "m", "r", "q", "d"],
     "voyelles"=>["O", "o", "e", "a", "i", "u"]
 );
 
     var_dump ($dic);
+
+
+
+    // for str index
+    // for string dans boucle consonnes
+    // for string dans boucle voyelles
+
+//     while (isset($str[$index])==true) {
+//     echo $str[$index];
+//     $index++;
+// }
 
 ?>
