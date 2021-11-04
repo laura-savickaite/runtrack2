ca1ee2246caa1be25e3a149e5ac788923ca79779
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-04 11:52:02 +0100

    job03 réalisé

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -3,6 +3,11 @@ $str="I'm sorry Dave I'm afraid I can't do that";
 
 $index=0; //mon index counter
 
+for($index=0; $str[$index]==true; $index++){
+    if($str[$index]== "a" || $str[$index]=="o" || $str[$index]=="I" || $str[$index]=="i" || $str[$index]=="e" || $str[$index]=="u" || $str[$index]=="y"){
+        echo $str[$index];}
+    }
+
 
 
 ?>
