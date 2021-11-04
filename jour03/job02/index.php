923d6508f4e87fa1f4e84700c4883dc843f28e66
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-04 11:31:29 +0100

    job02 done

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -4,4 +4,12 @@ $str="Tous ces instants seront perdus dans le temps comme les larmes sous la plu
 
 var_dump($str);
 
+$index=0; //mon index counter de char
+
+//echo $str[60];
+
+while (isset($str[$index*2])==true) {
+    echo $str[$index*2];
+    $index++;
+}
 ?>
