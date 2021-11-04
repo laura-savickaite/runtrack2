d973c2f7753860d37883fcbe0b985ed64689b6f8
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-04 14:08:07 +0100

    rajout après demande d'aide au niveau de la compréhension de mon code - qui est arrivé par hasard et tâtonement

--- jour03/job04/index.php
+++ jour03/job04/index.php
@@ -1,14 +1,16 @@
 <?php
 $str="Dans l'espace, personne ne vous entend crier";
 
-//$index=0; //c'est mon index counter de char
+$index=0; //c'est mon index counter de char
 
 // var_dump ($str); //à titre informatif pour savoir cb de char il y a dans ma string (pour mon résultat final) = 44
 
 while (isset($str[$index])==true) {
+    echo $str[$index];
     $index++;
 }
 
-echo $str. "<br/>" . $index;
+//on le sort puisque à la fin de la boucle while index n'est plus égal à 0 mais donc la valeur qu'il a compté dans la boucle
+echo "<br/>".$index;
 
 ?>
--- jour03/job05/index.php
+++ jour03/job05/index.php
@@ -1,3 +1,7 @@
 <?php
 
+$str="On n'est pas le meilleur quand on le croit mais quand on le sait";
+
+
+
 ?>
