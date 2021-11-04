c93dadb72766552ef537af18ccc0779546829801
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-04 10:54:57 +0100

    job 01 done

---
+++ jour03/job01/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$tableau=array(200, 204, 173, 98, 171, 404, 459);
+
+var_dump ($tableau);
+// echo $tableau[0];
+
+// $index=0; //mon index counter
+
+for($index=0; $tableau[$index]==true; $index++) {
+    if ($tableau[$index]%2==0){
+        echo $tableau[$index]. "est pair <br/>";
+    } else {
+        echo $tableau[$index]. "est impair<br/>";
+    }
+}
+
+
+?>
