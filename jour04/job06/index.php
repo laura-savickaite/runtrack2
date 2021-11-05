b72056bcf73ece0311d45141d920aca7d591cf63
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-05 15:03:31 +0100

    commencement de réflexion

--- jour04/job06/index.php
+++ jour04/job06/index.php
@@ -1,3 +1,17 @@
 <?php
+// $index=0;
 
+    if ($_GET["nombre"]%2==0){
+        echo $_GET["nombre"]. "est pair <br/>";
+    }else{
+        echo $_GET["nombre"]. "est impair <br/>";
+    }
 ?>
+
+<form action="index.php" method="post">
+    <div>
+        <label for="nombre">nombre :</label>
+        <input type="text" id="number" name="nombre">
+    </div>
+    <button type="submit">Submit</button>
+</form>
