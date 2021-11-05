9d0ca8dfda2db49a131298f8ab8bf167ba6f50a9
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-05 14:18:57 +0100

    job04 fini, job05 créé et son index

--- jour04/job04/index.php
+++ jour04/job04/index.php
@@ -1,3 +1,30 @@
-<?php
+<table>
+<thead>
+    <th>
+        arguments
+    </th>
+    <th>
+        valeurs
+    </th>
+</thead>
+<tbody>
+    <tr><td>username</td>
+    <td><?php echo $_POST ["username"]?></td></tr>
 
-?>
+    <tr><td>password</td>
+        <td><?php echo $_POST ["password"] ?></td></tr>
+        
+</tbody>
+</table>
+
+<form action="index.php" method="post">
+    <div>
+        <label for="nom">username :</label>
+        <input type="text" id="name" name="username">
+    </div>
+    <div>
+        <label for="prenom">password :</label>
+        <input type="password" id="pass" name="password">
+    </div>
+    <button type="submit">Submit</button>
+</form>
