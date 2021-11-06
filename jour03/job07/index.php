4308287c960b2c364eb16c302b0dbcaa37ca7d23
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-06 15:32:29 +0100

    tentative job07 jour03

--- jour03/job07/index.php
+++ jour03/job07/index.php
@@ -1,13 +1,47 @@
 <?php
 $str="Certaines choses changent, et d'autres ne changeront jamais.";
-$str2="";
 
-$index=0;
-while (isset($str[$index])==true) {
-    echo $str[$index];
-    $index++ ;
-}
-echo $index;
+
+
+for($index=0; isset($str[$index])==true; $index++){
+    $actuel=$str[$index];
+    $voisin=$str[$index+1];
+    echo $actuel;
+    echo $voisin;  
+
+    foreach ($str as $actuel=>$voisin){
+        echo $str;
+    }
+    
+    }
+   
+    
+
+    
+
+    
+    
+// $str2="";
+
+
+// $index=0;
+// while (isset($str[$index])==true) {
+//     echo $str[$index];
+//     $index++ ;
+// }
+// echo $index;
+
+//str de i est à 0
+//str de i+1 à 1
+
+//var_dump ($str);
+
+// foreach ($str as $key=>$index){
+    //     if($key != 'key3')$new[$key]=$index;
+    // }
+
+
+
 
 //une boucle qui trouve le dernier index
 //une boucle qui va décaller tout de +1
