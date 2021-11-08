a51970fb6086a243af8c946551c0afdd07781a6a
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-08 09:43:00 +0100

    job 01 réalisé

---
+++ jour05/job01/index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo hello (1);
+
+
+function hello ($hello){
+    if ($hello==1){
+        return ("Hello la Plateforme!");}
+    else {
+        return (0);
+     }
+     return (-1);
+}
+
+?>
