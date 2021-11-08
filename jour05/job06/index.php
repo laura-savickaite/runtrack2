9c69064d49938a051cfaff360cd518d479754110
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-08 13:15:27 +0100

    job06 terminé

--- jour05/job06/index.php
+++ jour05/job06/index.php
@@ -1,3 +1,29 @@
 <?php
 
+$str="BONJOUR JE SUIS LAURA un joyeux non anniversaire";
+leetspeak ($str);
+
+function leetspeak ($str) {
+    for($index=0; isset($str[$index])==true; $index++){
+
+        if (($str[$index])=="a" || ($str[$index])=="A"){
+            echo 4;
+        } elseif (($str[$index])=="b" || ($str[$index])=="B"){
+            echo 8;
+        } elseif (($str[$index])=="e" || ($str[$index])=="E"){
+            echo 3;
+        } elseif (($str[$index])=="g" || ($str[$index])=="G"){
+            echo 6;
+        } elseif (($str[$index])=="l" || ($str[$index])=="L"){
+            echo 1;
+        } elseif (($str[$index])=="s" || ($str[$index])=="S"){
+            echo 5;
+        } elseif (($str[$index])=="t" || ($str[$index])=="T"){
+            echo 7;
+        } else {
+            echo $str[$index];
+        }
+    }
+    return ($str);
+}
 ?>
