ed0d444c7aa5a661bdb409dc9a956d19745a9fcb
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-08 10:42:35 +0100

    job04 terminé

--- jour05/job04/index.php
+++ jour05/job04/index.php
@@ -1,7 +1,37 @@
 <?php
 
+$a=5;
+$b=6;
+echo calcule ($a, "-", $b);
 
-function calcule (a,operation,)
+
+
+
+function calcule ($a,$operation,$b) {
+    //$operation=(+)|| $operation=(-)|| $operation=(*) || $operation=(/) || $operation=(%));
+    // $taboperation= array ("+", "*", "-", "%", "/");
+    // for ($index=0; isset($taboperation[$index])==true; $index++) {
+    //     if ($taboperation[$index]==$operation){
+    //         return ($a + $operation +$b);
+    //     }
+    // }
+    if ($operation == "+"){
+        return ($a + $b);
+    }
+    elseif ($operation == "/"){
+        return ($a / $b);
+    }
+    elseif ($operation == "-"){
+        return ($a - $b);
+    }
+    elseif ($operation == "*"){
+        return ($a * $b);
+    }
+    elseif ($operation == "%"){
+        return ($a % $b);
+    }
+    return (-1);
+}
 
 
 ?>
