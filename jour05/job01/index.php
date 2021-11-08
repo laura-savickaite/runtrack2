255f9a747c1b94707d4b87e7e36e4e660c13028a
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-08 10:00:30 +0100

    création job04

--- jour05/job01/index.php
+++ jour05/job01/index.php
@@ -5,7 +5,8 @@ echo hello (1);
 
 function hello ($hello){
     if ($hello==1){
-        return ("Hello la Plateforme!");}
+        return ("Hello la Plateforme!");
+    }
     else {
         return (0);
      }
