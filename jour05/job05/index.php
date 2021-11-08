8ec7caa0682929021d9cfd5b9a82e88698efdfe7
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-08 11:30:57 +0100

    job05 terminé

--- jour05/job05/index.php
+++ jour05/job05/index.php
@@ -1,10 +1,27 @@
 <?php
 
-$str="salut c'est laura"
+$str="salut c'est laura";
+$char="c";
+echo $str. "<br/>";
+echo occurrences ($str, $char);
 
 
 function occurrences ($str, $char) {
-    
+    for($index=0; isset($str[$index])==true; $index++){
+            $nbchar=0;
+
+        for($totalchar=0; isset($str[$totalchar])==true; $totalchar++){
+            if (($str[$totalchar])==$char){
+                $nbchar++;
+            } 
+        } 
+
+        
+        return ($nbchar);
+        } 
 }
+
+
+//NOTES POUR MOI-MM: quand j'ai fait for($char=0; isset($str[$char])==true; $char++) au lieu de totalchar au départ => il me donnait l'entièreté des charactères présents dans ma chaines (c'était sans le IF que j'ai rajouté après !!!!! juste le $nbchar après) : je devais donc rajouter une condition que si dans la totalité de ces charactères il y en avait 1 qui ressemblait à la valeur que je mettais dans $char ALORS le compteur rajoutait +1
 
 ?>
