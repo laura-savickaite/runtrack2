b627c72ed43ebd539402e797d0a04d29f5a002b2
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-09 10:11:40 +0100

    job02 terminé

--- jour07/job02/index.php
+++ jour07/job02/index.php
@@ -1,6 +1,16 @@
 <?php
 
-setcookie('nbvisites')
+setcookie('nbvisites', $_COOKIE['nbvisites']+1, time()+365*24*3600);
 
+echo $_COOKIE['nbvisites'];
 
+if (isset($_POST["reset"])){
+    setcookie('nbvisites');
+}else {
+    $_COOKIE['nbvisites'];
+}
 ?>
+
+<form action="index.php" method="post">
+<input type="submit" value="Reset" name="reset">
+</form>
