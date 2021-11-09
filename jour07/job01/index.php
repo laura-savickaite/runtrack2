17f5164148b000efa7e5064a9ea1641745beba18
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-09 09:53:14 +0100

    job01 fait

--- jour07/job01/index.php
+++ jour07/job01/index.php
@@ -1,6 +1,18 @@
 <?php
 session_start();
 
-$_SESSION['nbdevisites']=''
+$_SESSION['nbdevisites']=$_SESSION['nbdevisites']+1;
+
+echo $_SESSION['nbdevisites'];
+
+if (isset($_POST["reset"])){
+    session_destroy();
+}else {
+    $_SESSION['nbdevisites'];
+}
 
 ?>
+
+<form action="index.php" method="post">
+<input type="submit" value="Reset" name="reset">
+</form>
