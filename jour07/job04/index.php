0a252e375dee7a7c0ba28f98e0ecaeeb1c8261fb
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-09 14:25:01 +0100

    job04 terminé

--- jour07/job04/index.php
+++ jour07/job04/index.php
@@ -1,29 +1,33 @@
 <?php
 
-setcookie('prenom', $_POST['prenom'], time()+365*24*3600);
-
-if (isset($_POST["deco"])){
-     //setcookie('prenom');
-     echo '<style>
-        #formulaire {
-            visibility:visible;
-        }
-        </style>';
-}
 
 if (isset($_POST["connexion"]))
  {
-    if ($_COOKIE['prenom']==$_POST['prenom'])
-     {
-    echo "Bonjour " . $_COOKIE['prenom'] . " ! <br><input type='submit' value='deconnexion' name='deco'>
-    <style>
-        #formulaire {
-            visibility:hidden;
-        }
-    </style>";
-    
+    setcookie('prenom', $_POST['prenom'], time()+365*24*3600);
+    $_COOKIE['prenom'] = $_POST['prenom'];
+    echo "Bonjour " . $_COOKIE['prenom'] . " ! <br>"; 
+    echo "<style>
+    #formulaire{
+        visibility:hidden;
     }
-} 
+    #deco{
+        visibility:visible;
+    }
+    </style>
+    ";
+}
+
+    
+    
+if (isset($_POST["deco"])){
+    setcookie('prenom');
+    echo "<style>
+    #deco{
+        visibility:hidden;
+    }
+    </style>
+    ";
+   } 
 
 ?>
 
@@ -33,5 +37,7 @@ if (isset($_POST["connexion"]))
             <input type="text" id="prenom" name="prenom">
             <input type="submit" value="connexion" name="connexion">
     </div>
+
+    <div id="deco"><input type='submit' value='deconnexion' name='deco'></div>
     
 </form>
