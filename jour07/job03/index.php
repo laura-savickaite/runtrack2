deb1d275493e4817c392cf47275c483438f8a39b
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-09 10:48:04 +0100

    job03 terminé

--- jour07/job03/index.php
+++ jour07/job03/index.php
@@ -1,3 +1,24 @@
 <?php
 
+session_start();
+
+$_SESSION['prenom'] .= ' '.$_POST['prenom'];
+
+echo $_SESSION['prenom'];
+
+var_dump ($_SESSION['prenom']);
+
+if (isset($_POST["reset"])){
+    session_destroy();
+}
+
 ?>
+
+<form action="index.php" method="post">
+    <div>
+        <label for="texte">prénom :</label>
+            <input type="text" id="prenom" name="prenom">
+    </div>
+    <input type="submit" value="submit" name="submit">
+    <input type="submit" value="reset" name="reset">
+</form>
