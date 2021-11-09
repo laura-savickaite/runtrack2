67591aed664c9eee677c7d53e1a5733069681e52
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-09 10:53:00 +0100

    création du job04 et son index ainsi qu'apport de précisions pour le job03

--- jour07/job03/index.php
+++ jour07/job03/index.php
@@ -3,6 +3,10 @@
 session_start();
 
 $_SESSION['prenom'] .= ' '.$_POST['prenom'];
+// .= permet de concatener DONC enchainer deux idées/strings entre elles (ne marche que sur les strings (?)) :: .= concatenating assignment operator ::: ex : 
+//$a = "Bonjour ";
+//$a .= "Monde !"; // $a contient "Bonjour Monde !"
+//ici dans le truc plus haut pas besoin de réécrire $_SESSION['prenom']= etc... PARCE QUE on veut exactement la même chose/la même variable (mais la valeur peut changer selon ce que met l'utilisateur) pour la succession contrairement à l'exemple avec $a où on veut rajouter une autre valeur.
 
 echo $_SESSION['prenom'];
 
