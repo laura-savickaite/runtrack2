0be9bff7ce77ea88ea3545e44394338ee3bc1320
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-12 10:16:13 +0100

    job02

--- jour09/job02/index.php
+++ jour09/job02/index.php
@@ -1,3 +1,41 @@
 <?php
 
+$connect = mysqli_connect('localhost', 'root', '', 'jour08');
+
+mysqli_set_charset($connect,"utf8");
+
+$salles = mysqli_query ($connect, "SELECT `nom`, `capacite` FROM `salles`");
+
+$recup = mysqli_fetch_all($salles, MYSQLI_ASSOC);
+
+//echo "<pre>";var_dump ($recup);echo "</pre>";
+
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>mysqli</title>
+</head>
+
+<body>
+<table>
+    <thead>
+        <tr>
+            <th>nom</th>
+            <th>capacite</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        foreach ($salles as $value){
+            echo "<tr><td>". $value ['nom'] ."</td>";
+            echo "<td>". $value ['capacite'] ."</td></tr>";
+        }
+        ?>
+    </tbody>
+</table>
+</body>
+
+</html>
+
