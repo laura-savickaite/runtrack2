35f306ca997164f2f3f8fd6f57faeeed0fe22723
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-15 15:59:43 +0100

    job07 commencé

---
+++ jour06/job07/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$tab=array("abc", "ghi", "def");
+$croissant=true;
+echo bubblesort($stab,$croissant);
+
+function bubblesort ($tab,$croissant){
+    for ($index=0; isset($tab[$index])==true; $index++){
+        if ($croissant=true){
+            
+    }
+    return (-1);
+}
+
+?>
